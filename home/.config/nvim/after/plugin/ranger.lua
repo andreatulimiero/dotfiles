8d0cebc155292b4e3c02b780aecb8ab02544f734
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2023-09-25 21:58:20 +0200

    Fixed lua ranger plugin setup

--- home/.config/nvim/after/plugin/ranger.lua
+++ home/.config/nvim/after/plugin/ranger.lua
@@ -1,5 +1,7 @@
 local ranger_nvim
-local success, err = pcall(require, "ranger-nvim")
+local success, err = pcall(function ()
+  ranger_nvim = require("ranger-nvim")
+end)
 if not success then
   print("Ranger module not found, aborting ranger.lua execution")
   return
