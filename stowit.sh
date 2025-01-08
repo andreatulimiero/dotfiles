78cc74597bc16f15e8eb9b83221c8f8473303a88
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2025-01-08 21:51:32 +0100

    Cleaned up stowit.sh logs

--- stowit.sh
+++ stowit.sh
@@ -6,11 +6,13 @@ branch=$(GetHostname)
 Log "Versioning configs by switching to ${branch} branch ..."
 git checkout ${branch} &>/dev/null
 CheckSuccessOrExitWith "Failed to switch to ${branch} branch"
-Success "Versioned configs"
 
 # Stow the home folder
+Log "Stowing configs ..."
 stow home -t ~/
 
 # Check out back to main
-Log "Checking back out to main"
+Log "Checking back out to main ..."
 git checkout main
+
+Success "Done"
