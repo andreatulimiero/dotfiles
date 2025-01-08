3d0a29f165655c872cb439733d647b883742e64f
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2025-01-08 21:49:18 +0100

    Update stowit.sh script to use new Git based versioning

--- stowit.sh
+++ stowit.sh
@@ -2,7 +2,15 @@
 source base.sh
 
 # Prepare the config files
-./prepare.py
+branch=$(GetHostname)
+Log "Versioning configs by switching to ${branch} branch ..."
+git checkout ${branch} &>/dev/null
+CheckSuccessOrExitWith "Failed to switch to ${branch} branch"
+Success "Versioned configs"
 
 # Stow the home folder
 stow home -t ~/
+
+# Check out back to main
+Log "Checking back out to main"
+git checkout main
